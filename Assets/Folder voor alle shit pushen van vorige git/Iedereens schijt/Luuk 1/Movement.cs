05e582a68fdabf85393356d09cc3faf9b069e51d
luuk boersma <49709526+Kluunkert@users.noreply.github.com>
2020-01-16 13:53:44 +0100

    .cs

---
+++ Assets/Folder voor alle shit pushen van vorige git/Iedereens schijt/Luuk 1/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public Vector3 dir;
+
+    public float movementSpeed;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Physics.IgnoreLayerCollision(8, 9);    
+    }
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    public void PlayerMove()
+    {
+        float horiInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        dir = new Vector3(horiInput, 0, vertInput);
+
+        transform.Translate(dir * movementSpeed * Time.deltaTime);
+    }
+
+}
