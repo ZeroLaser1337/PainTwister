8619defb263b605fa7b88457fe4558c09a8482c9
Ryusake Weiss <ryuweiss@gmail.com>
2020-01-16 20:19:43 +0100

    sadasd

--- Assets/FirstPersonControllerbuild/Scripts/Destination.cs
+++ Assets/FirstPersonControllerbuild/Scripts/Destination.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class Destination : MonoBehaviour
 {
-    public string myName;
+    public KnowledgeMeter knowledgeMeter;
+    public bool backHome;
+    public string myName,myNameBad,myNameGood;
 
     public void OnCollisionEnter(Collision c)
     {        
         if(c.gameObject.tag == "Player")
         {
-            print(c.gameObject);
-            c.gameObject.GetComponent<DoorTeleport>().NewDestination(myName);
+            if (backHome)
+            {
+                if(knowledgeMeter.knowledgeAmount < 30)
+                {
+                    c.gameObject.GetComponent<DoorTeleport>().NewDestination(myNameBad);
+                }
+
+                else if(knowledgeMeter.knowledgeAmount > 30 && knowledgeMeter.knowledgeAmount < 90)
+                {
+                    c.gameObject.GetComponent<DoorTeleport>().NewDestination(myName);
+                }
+
+                else if(knowledgeMeter.knowledgeAmount >= 90)
+                {
+                    c.gameObject.GetComponent<DoorTeleport>().NewDestination(myNameGood);
+                }
+            }
+            else
+            {
+                c.gameObject.GetComponent<DoorTeleport>().NewDestination(myName);
+            }
         }        
     }
 }
