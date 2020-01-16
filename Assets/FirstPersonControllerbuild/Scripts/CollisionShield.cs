204ffe173dbc718078afd93c6851d46a08d68eac
Ryusake Weiss <ryuweiss@gmail.com>
2020-01-16 18:43:54 +0100

    aafas

---
+++ Assets/FirstPersonControllerbuild/Scripts/CollisionShield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionShield : MonoBehaviour
+{
+    public GameObject me;
+
+    public GameObject target;
+
+    public void Start()
+    {
+        Physics.IgnoreLayerCollision(8, 9);
+    }
+
+    void Update()
+    {
+        me.transform.position = target.transform.position;
+    }
+}
