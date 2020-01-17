4b1bbd299e34050483488af5185cf530588dc555
wtfRowin <r.s.salomons@gmail.com>
2020-01-17 12:37:45 +0100

    fasf

---
+++ Assets/Folder voor alle shit pushen van vorige git/Iedereens schijt/Rowins Meuk/General/BoxHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxHolder : MonoBehaviour
+{
+    //private PointManager points;
+    private Renderer rend;
+    protected bool used;
+
+    public bool small, medium, big;
+
+    void Start()
+    {
+        rend = this.GetComponent<Renderer>();
+        //points = GameObject.Find("PointSystem").GetComponent<PointManager>();
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        if(used == false)
+        {
+            CheckBox(coll);
+        }
+    }
+    void CheckBox(Collider collision)
+    {
+        if(small == true && collision.gameObject.CompareTag("Small Box"))
+        {
+            EnableBox(collision);
+        }
+        if(medium == true && collision.gameObject.CompareTag("Medium Box"))
+        {
+            EnableBox(collision);
+        }
+        if(big == true && collision.gameObject.CompareTag("Big Box"))
+        {
+            EnableBox(collision);
+        }
+    }
+    void EnableBox(Collider coll)
+    {
+        Destroy(coll.gameObject);
+        rend.enabled = true;
+        used = true;
+    }
+}
---
+++ Assets/Folder voor alle shit pushen van vorige git/Iedereens schijt/Rowins Meuk/General/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Teleport : MonoBehaviour
+{
+   void Update()
+   {
+       if(Input.GetButtonDown("Oculus_CrossPlatform_Button2"))
+       {
+           SceneManager.LoadScene("Consult");
+       }
+       if(Input.GetButtonDown("Oculus_CrossPlatform_Button4"))
+       {
+           SceneManager.LoadScene("Storage Room");
+       }
+   }
+}
